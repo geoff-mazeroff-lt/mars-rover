10a8666c4b1a1ad6bd5f6f1d8632cd15a53a42f2
geoff-mazeroff-lt <gmazeroff@leantechniques.com>
2025-03-11 17:14:20 -0400

    created Rover class and ctor test

---
+++ src/models.spec.ts
@@ -0,0 +1,12 @@
+﻿import {Rover} from "./models";
+
+describe("rover model", () => {
+    
+    it("rover is at (0,0) facing north", () => {
+        const rover:Rover = new Rover(0, 0, 'north');
+        expect(rover.x).toBe(0);
+        expect(rover.y).toBe(0);
+        expect(rover.direction).toBe('north');
+    })
+    
+})
---
+++ src/models.ts
@@ -0,0 +1,38 @@
+﻿// This is a sum-type / union-type
+type Direction = 'north' | 'south' | 'east' | 'west';
+
+// Rover as a class -- more common than the verbose version above, also found in Angular that expects classes
+export class Rover {
+    x:number;
+    y:number;
+    direction:Direction;
+    
+    constructor(x:number, y:number, direction:Direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
+
+
+
+
+// Rover as just a type -- more functional style.
+// type Rover = {
+//     x:number;
+//     y:number;
+//     direction:Direction;
+// }
+
+// Very verbose as a class!
+// class Rover {
+//     private readonly x:Number;
+//     private readonly y:Number;
+//     private readonly direction:Direction;
+//    
+//     constructor(x:number, y:number, direction:Direction){
+//         this.x = x;
+//         this.y = y;
+//         this.direction = direction;
+//     }
+// }
